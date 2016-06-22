083fb54dcb5b2a3f2d9a8e9d1041de27d5d1d369
evelyngonzalez89 <evengonz@espol.edu.ec>
2016-06-22 18:08:26 -0500

    Java ModuloServidor SO  por Evelyn Gonzalez

---
+++ SimpleServer.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+public class SimpleServer
+{
+    public static void main(String args[]) 
+    {
+        ServerSocket s;
+        // Register service to port 5432 or anyother number
+        try
+        {
+            s = new ServerSocket(5432);
+        } 
+        catch (IOException e) 
+        {
+            s = null; 
+            e.printStackTrace();
+        }
+ 
+        while (true) 
+        {
+            try
+            {
+                 // Wait here and listen for a connection
+                 Socket s1 = s.accept();
+                 // Get output stream associated with the socket
+                 OutputStream s1out = s1.getOutputStream();
+                 DataOutputStream dos = new DataOutputStream(s1out);
+                 // Send your string!
+                 dos.writeUTF("Hello Net World!");
+                 // Close the connection, but not the server socket
+                 dos.close();
+                 s1.close();
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace();
+            }
+        } //end of while
+    }//end of main
+}//end of class
