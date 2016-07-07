6ce6e5116f9fddcddf4532ab9142ae2d6e874ce2
evelyngonzalez89 <evengonz@espol.edu.ec>
2016-07-07 15:21:54 -0500

    07/07/2016 CurrentHashMap OPERTIVOS

--- src/servidor/KVKeyValuehasp.java
+++ src/servidor/KVKeyValuehasp.java
@@ -1,19 +1,18 @@
 package servidor;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class KVKeyValuehasp {
-
 	/**
 	 * @param args
 	 */
 		    public String key;       public String valor;   
-		    private Map<String, String> map = new HashMap<String,String>(); 
+		    private ConcurrentHashMap<String, String> map = new ConcurrentHashMap<String,String>(); 
 		 
 		    public KVKeyValuehasp(){
-		    	
 		    }
 
 		    public KVKeyValuehasp(String key, String valor)
@@ -21,7 +20,7 @@ public class KVKeyValuehasp {
 		         { this.key = key; this.valor = valor;}
 		    
 		    public void putKeyValue(String key, String valor){
-		    	map.put(key, valor);
+		    	map.putIfAbsent(key, valor);
 		    }
 
 		    public String getKeyValue(String key){
