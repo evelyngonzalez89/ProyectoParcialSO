e8cfa8e391e5902ffd363d7d6ffac352af88b4ab
evelyngonzalez89 <evengonz@espol.edu.ec>
2016-08-25 15:54:26 -0500

    Presentacion FInal

---
+++ bash_leerLinea.sh
@@ -0,0 +1,52 @@
+ cat h6lectura61.txt 
+ cat h6lectura62.txt 
+ cat h6lectura63.txt
+ cat h6lectura64.txt
+ cat h6lectura65.txt
+ cat h7lectura71.txt
+ cat  h7lectura72.txt
+ cat  h7lectura73.txt
+ cat  h7lectura74.txt
+ cat  h7lectura75.txt
+ cat  loadlectura11.txt
+ cat  loadlectura12.txt
+ cat  loadlectura13.txt
+ cat  loadlectura14.txt
+ cat  loadlectura15.txt
+ cat  loadlectura21.txt
+ cat  loadlectura22.txt
+ cat  loadlectura23.txt
+ cat  loadlectura24.txt
+ cat  loadlectura25.txt
+ cat  loadlectura31.txt
+ cat  loadlectura32.txt
+ cat  loadlectura33.txt
+ cat  loadlectura34.txt
+ cat  loadlectura35.txt
+ cat  loadlectura41.txt
+ cat  loadlectura42.txt
+ cat  loadlectura43.txt
+ cat  loadlectura44.txt
+ cat  loadlectura45.txt
+ cat  loadlectura51.txt
+ cat loadlectura52.txt
+ cat  loadlectura53.txt
+ cat loadlectura54.txt
+ cat  loadlectura55.txt
+ cat  loadlectura81.txt
+ cat  loadlectura82.txt
+ cat  loadlectura83.txt
+ cat  loadlectura84.txt
+ cat  loadlectura85.txt
+ cat  loadlectura101.txt
+ cat  loadlectura102.txt
+ cat  loadlectura103.txt
+ cat  loadlectura104.txt
+ cat  loadlectura105.txt
+ 
+
+
+
+
+ 
+ 
